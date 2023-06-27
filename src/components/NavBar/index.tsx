94e5d9a3e8eb4edb863f481396668c57637b96c5
Junior <juniorfernandessilva1@gmail.com>
2023-06-26 22:30:23 -0300

    Section NavBar added

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,58 @@
+import styles from './NavBar.module.scss'
+import logo from '../../assets/img/logo.svg'
+import menu from '../../assets/img/bx-menu.svg'
+import classNames from 'classnames';
+import { useState, useEffect } from 'react';
+
+
+
+
+ const NavBar = ()=>{
+    const [activeLink, setActiveLink] = useState<string>('home');
+
+    const onUpdateActiveLink = (value: string) => {
+        setActiveLink(value)
+    }
+    return(
+        <div className={styles.header}>
+            
+            <a href='#home' className={styles.navbarLogo} onClick={()=> onUpdateActiveLink('home')}><img className={styles.logo} src={logo} alt='Logo'/></a>
+            
+            <img src={menu} id={styles.menu}/>
+            
+            <nav className={classNames([styles.navbar])}>
+                <a 
+                    href='#home'
+                    className={classNames({
+                        [styles.active]: activeLink === 'home'
+                    })}
+                    onClick={()=> onUpdateActiveLink('home')}
+                >Home</a>
+                <a 
+                    href='#about'
+                    className={classNames({
+                        [styles.active]: activeLink === 'about'
+                    })}
+                    onClick={()=> onUpdateActiveLink('about')}
+                >About</a>
+                <a 
+                    href='#projects'
+                    className={classNames({
+                        [styles.active]: activeLink === 'projects'
+                    })}
+                    onClick={()=> onUpdateActiveLink('projects')}
+                >Projects</a>
+                <a 
+                    href='#contact'
+                    className={classNames({
+                        [styles.active]: activeLink === 'contact'
+                    })}
+                    onClick={()=> onUpdateActiveLink('contact')}
+                >Contact</a>
+            </nav>
+
+        </div>
+    )
+}
+
+export default NavBar;
