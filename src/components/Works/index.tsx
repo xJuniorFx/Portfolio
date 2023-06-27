5f8b0936f497e375294d8db1a0a06cec8f3f8536
Junior <juniorfernandessilva1@gmail.com>
2023-06-26 22:29:59 -0300

    Section Works added

---
+++ src/components/Works/index.tsx
@@ -0,0 +1,19 @@
+
+import styles from './Works.module.scss'
+import Projects from './projects.json'
+
+const Works = () => {
+    return(
+    
+        <section className={styles.projects} id='projects'>
+            <h1 className={styles.title}>Latest <span> Projects</span></h1>
+
+            <div className={styles.container}>
+                
+            </div>
+        </section>
+    
+    )
+}
+
+export default Works;
