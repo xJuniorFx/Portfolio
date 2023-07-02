a6e0a94346c813105639878c8114a2c8925803a0
Junior <juniorfernandessilva1@gmail.com>
2023-07-02 18:48:00 -0300

    Pagina Banner finalizada + responsividade

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,4 +1,4 @@
-
+import Gif from '../../assets/img/Program.svg'
 import github from '../../assets/img/github.svg'
 import linkedin from '../../assets/img/linkedin.svg'
 import styles from './Banner.module.scss'
@@ -8,28 +8,38 @@ const Banner = ()=> {
     return(
         
             <section className={styles.home} id='home'>
-                
+        
                 <div className={styles.content}>
-                    <h1>Hi, I'm Junior Fernandes</h1>
-                    <div className={styles.animatedText}>
-                        <h2>Front-End Developer</h2>
+                    <div className={styles.left_content}>
+                        <h1>Hi, I'm Junior Fernandes</h1>
+                        <div className={styles.content_destaque}>
+                            <div className={styles.animated_text}>
+                                <h2 className={styles.destaque}>Front-End Developer</h2>
+                            </div>
+                            <div className={styles.social_network}>
+                                <a href='https://github.com/xJuniorFx' target="_blank" rel="noreferrer"><img className={styles.network} alt='Github logo' src={github}></img></a>
+                                <a href='https://www.linkedin.com/in/juniorfs/' target="_blank" rel="noreferrer"><img className={styles.network} alt='Github logo' src={linkedin} ></img></a>
+                            </div>
+                        </div>
+                        <div className={styles.resume}>
+                            <p>
+                                I'm a proactive and self-motivated individual who enjoys taking on new challenges to expand my 
+                                skills. I thrive in team environments, valuing clear communication and transparency. 
+                            </p>
+                            <p>
+                                With the ability to work independently, I ensure projects are completed even under tight deadlines. Explore
+                                my portfolio to see the results of my dedicated efforts.
+                            </p>
+                        </div>
+                        <div className={styles.anchor_box} id={styles.anchoreffect}>
+                            <a href='#projects' className={styles.anchor}>Projects</a>
+                            <a href='#contact' className={styles.anchor}>Contact</a>
+                        </div>
+                        
                     </div>
-                    <p>
-                        I'm a proactive and self-motivated individual who enjoys taking on new challenges to expand my 
-                        skills. I thrive in team environments, valuing clear communication and transparency. With the 
-                        ability to work independently, I ensure projects are completed even under tight deadlines. Explore
-                        my portfolio to see the results of my dedicated efforts.
-                    </p>
-                    <div className={styles.anchorBox} id={styles.anchoreffect}>
-                        <a href='#projects' className={styles.anchor}>Projects</a>
-                        <a href='#contact' className={styles.anchor}>Contact</a>
+                    <div className={styles.right_content}>   
+                        <img src={Gif} alt='imagem'/>
                     </div>
-                    <div className={styles.socialNetwork}>
-                        <a href='https://github.com/xJuniorFx' target="_blank" rel="noreferrer"><img className={styles.network} alt='Github logo' src={github}></img></a>
-                        <a href='https://www.linkedin.com/in/juniorfs/' target="_blank" rel="noreferrer"><img className={styles.network} alt='Github logo' src={linkedin} ></img></a>
-                    </div>
-                    
-                    <span className={styles.homeHover}></span>
                 </div>
                 
             </section>
