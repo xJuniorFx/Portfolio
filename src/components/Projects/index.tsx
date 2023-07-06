7cce99303a43520e3be2f6572519f7915ad9b276
Junior <juniorfernandessilva1@gmail.com>
2023-07-06 01:25:49 -0300

    Atualizando json keys

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -9,8 +9,8 @@ const Projects = () => {
             <h1 className={styles.title}>Latest <span> Projects</span></h1>
 
             <div className={styles.projects_container}>
-                {projects.map(project => (
-                    <Card key={project.title} {...project} />
+                {projects.map((project, index) => (
+                    <Card key={index} {...project} />
                 ))}
             </div>
         </section>
