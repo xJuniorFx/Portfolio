a68b28627d954361c5ee24c0e72d98569c0f2d3b
Junior <juniorfernandessilva1@gmail.com>
2023-07-07 00:53:26 -0300

    Atualizando NavBar resposividade + handling scroll event

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,18 +1,64 @@
 import styles from './NavBar.module.scss'
 import logo from '../../assets/img/logo.svg'
 import classNames from 'classnames';
-import { useState} from 'react';
+import { useState, useEffect} from 'react';
 
  const NavBar = ()=>{
+
     const [activeLink, setActiveLink] = useState<string>('home');
 
     const onUpdateActiveLink = (value: string) => {
         setActiveLink(value)
     }
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const homeElement = document.getElementById('home');
+            const aboutElement = document.getElementById('about');
+            const projectsElement = document.getElementById('projects');
+            const contactElement = document.getElementById('contact');
+
+            const scrollPosition = window.scrollY;
+
+                if (
+                homeElement && scrollPosition >= homeElement.offsetTop && aboutElement && scrollPosition < aboutElement.offsetTop &&
+                activeLink !== 'home'
+            ) {
+                setActiveLink('home');
+            } else if (
+                aboutElement && scrollPosition >= aboutElement.offsetTop && projectsElement && scrollPosition < projectsElement.offsetTop &&
+                activeLink !== 'about'
+            ) {
+                setActiveLink('about');
+            } else if (
+                projectsElement && scrollPosition >= projectsElement.offsetTop && contactElement && scrollPosition < contactElement.offsetTop &&
+                activeLink !== 'projects'
+            ) {
+                setActiveLink('projects');
+            } else if (
+                contactElement && scrollPosition >= contactElement.offsetTop &&
+                activeLink !== 'contact'
+            ) {
+                setActiveLink('contact');
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [activeLink]);
+
     return(
         <div className={styles.header}>
             
-            <a href='#home' className={styles.navbarLogo} onClick={()=> onUpdateActiveLink('home')}><img className={styles.logo} src={logo} alt='Logo'/></a>
+            <a 
+                href='#home' 
+                className={styles.navbarLogo} 
+                onClick={()=> onUpdateActiveLink('home')}
+
+            ><img className={styles.logo} src={logo} alt='Logo'/></a>
             
             
             <nav className={classNames([styles.navbar])}>
@@ -22,6 +68,7 @@ import { useState} from 'react';
                         [styles.active]: activeLink === 'home'
                     })}
                     onClick={()=> onUpdateActiveLink('home')}
+
                 >Home</a>
                 <a 
                     href='#about'
@@ -29,6 +76,7 @@ import { useState} from 'react';
                         [styles.active]: activeLink === 'about'
                     })}
                     onClick={()=> onUpdateActiveLink('about')}
+
                 >About</a>
                 <a 
                     href='#projects'
@@ -36,6 +84,7 @@ import { useState} from 'react';
                         [styles.active]: activeLink === 'projects'
                     })}
                     onClick={()=> onUpdateActiveLink('projects')}
+
                 >Projects</a>
                 <a 
                     href='#contact'
@@ -43,6 +92,7 @@ import { useState} from 'react';
                         [styles.active]: activeLink === 'contact'
                     })}
                     onClick={()=> onUpdateActiveLink('contact')}
+
                 >Contact</a>
             </nav>
 
