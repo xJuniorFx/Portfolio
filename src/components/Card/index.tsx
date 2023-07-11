b7287d38ace9d74f374ca54417d6828050c5e928
Junior <juniorfernandessilva1@gmail.com>
2023-07-10 23:41:48 -0300

    Image links

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,23 +1,25 @@
 import styles from './Card.module.scss'
 import gitHub from '../../assets/img/githubP.svg'
 import link from '../../assets/img/external-link.svg'
-import img from '../../assets/img/maintenance.png'
+
 
 interface CardProps{
     title: string; 
     description: string; 
-    linkGit: string; 
+    linkGit: string;
+    linkProject: string;
+    imgLink: string;
 }
 
 const Card = (props: CardProps)=>{
 
-    const {title, description, linkGit} = props;
+    const {title, description, linkGit, linkProject, imgLink} = props;
 
     return(
         
         <article className={styles.project}>
             <div className={styles.project_box}>
-                <img className={styles.project_image} src={img} alt={title} />
+                <img className={styles.project_image} src={imgLink} alt={title} />
                 <div className={styles.layer}>
                     <div className={styles.project_about}>
                         <h3 className={styles.project_title}>{title}</h3>
@@ -25,7 +27,7 @@ const Card = (props: CardProps)=>{
                     </div>
                     <div className={styles.link_section}>
                         <a className={styles.link} target="_blank" href={linkGit} rel="noreferrer"><img src={gitHub} alt="Githubb Icon" /></a>
-                        <a className={styles.link} target="_blank" href={linkGit} rel="noreferrer"><img src={link} alt='External link'/></a> 
+                        <a className={styles.link} target="_blank" href={linkProject} rel="noreferrer"><img src={link} alt='External link'/></a> 
                     </div>                
                 </div>
             </div>
